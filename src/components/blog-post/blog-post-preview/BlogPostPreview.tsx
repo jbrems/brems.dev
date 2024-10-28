aa4e95861782a8243afcdb0b8097dc2330d999a9
jbrems <jonasbrems@gmail.com>
2024-10-28 23:13:45 +0100

    Fix build

--- src/components/blog-post/blog-post-preview/BlogPostPreview.tsx
+++ src/components/blog-post/blog-post-preview/BlogPostPreview.tsx
@@ -1,3 +1,4 @@
+import { BlogPost } from '@/app/blog/blog.types'
 import styles from './BlogPostPreview.module.css'
 
 export function BlogPostPreview({ blogPost, children }: { blogPost: BlogPost, children?: React.ReactNode }) {
