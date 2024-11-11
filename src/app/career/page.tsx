6e5127e073679466451dbbab2882122ad1aa5f28
jbrems <jonasbrems@gmail.com>
2024-11-11 14:58:23 +0100

    Career page and current position

--- src/app/career/page.tsx
+++ src/app/career/page.tsx
@@ -1,36 +1,57 @@
 import { Timeline } from "@/components/timeline/Timeline";
 import styles from './page.module.css'
 import { CareerBlock } from "@/components/career-block/CareerBlock";
+import { LinkedinIcon } from "@/components/icons/LinkedinIcon";
+import Link from "next/link";
 
 export default function CareerPage() {
   return <>
-    <h2>My career</h2>
+    <div className={styles.title}>
+      <h2>My career</h2>
+      <Link href="https://www.linkedin.com/in/jonas-brems-a2022b2a/" target="_blank"><LinkedinIcon /></Link>
+    </div>
     <div className={styles.careerTimeline}>
       <Timeline />
       <div className={styles.career}>
-        <CareerBlock title="Web developer - De Lijn" period={['November 2023', 'now']} technologies={['Next.js', 'Azure', 'Docusaurus']} className={styles.deLijn}>
-          <p>At De Lijn I'm currently working as part of the web team, extending and improving the company's website and public facing applications.</p>
-        </CareerBlock>
-        <article className={styles.thailand}></article>
-        <CareerBlock title="Web developer - Customs4Trade" period={['April 2022', 'October 2023']} technologies={['Angular', 'Cypress', 'Storybook', 'Azure']} className={styles.c4t}>
-          <p>At Custums4Trade I spearheaded the front end development for the company's SAAS application in a vast team of backend developers.</p>
-          <p>As the only front-end developer my responsibilities included managing the Angular code base and teaching my colleagues some web application development.</p>
-        </CareerBlock>
-        <CareerBlock title="Web developer - Christelijke Mutualiteiten" period={['May 2017', 'April 2022']} technologies={['Angular', 'Node.js', 'MongoDB', 'OAuth', 'Jest', 'Cypress']} className={styles.cmWeb}>
-          <p>After 5 years of Java development I joined CM's web team as a junior,
-self-taught Angular and Nodejs
-developer and contributed to making
-web applications to be embedded in the company's website.</p>
-          <p>Through the years I became the most experienced member of the
-team where I supported my colleagues and tackled the most
-challenging tasks as a senior developer and solution architect.</p>
-        </CareerBlock>
-        <CareerBlock title="Java developer - Christelijke Mutualiteiten" period={['April 2012', 'May 2017']} technologies={['Java EE', 'Struts']} className={styles.cmJava}>
-          <p>At CM I gained my first professional software development
-experience while being part of various experienced teams creating
-Java backends and internal web applications for processes within CM.</p>
-        </CareerBlock>
+        <DeLijnBlock />
+        <div className={styles.thailand}></div>
+        <C4TBlock />
+        <CmWebBlock />
+        <CmJavaBlock />
       </div>
     </div>
   </>
+}
+
+export function DeLijnBlock() {
+  return <CareerBlock title="Web developer - De Lijn" period={['November 2023', 'current']} technologies={['Next.js', 'Azure', 'Docusaurus']} className={styles.deLijn}>
+    <p>At De Lijn I'm currently working as part of the web team, extending and improving the company's website functionalities and public facing applications.</p>
+  </CareerBlock>
+}
+
+function C4TBlock() {
+  return <CareerBlock title="Web developer - Customs4Trade" period={['April 2022', 'October 2023']} technologies={['Angular', 'Cypress', 'Storybook', 'Azure']} className={styles.c4t}>
+    <p>At Custums4Trade I spearheaded the front end development for the company's SAAS application in a vast team of backend developers.</p>
+    <p>As the only front-end developer my responsibilities included managing the Angular code base and teaching my colleagues some web application development.</p>
+  </CareerBlock>
+}
+
+function CmWebBlock() {
+  return <CareerBlock title="Web developer - CM" period={['May 2017', 'April 2022']} technologies={['Angular', 'Node.js', 'MongoDB', 'OAuth', 'Jest', 'Cypress']} className={styles.cmWeb}>
+    <p>After 5 years of Java development I joined CM's web team as a junior,
+      self-taught Angular and Nodejs
+      developer and contributed to making
+      web applications to be embedded in the company's website.</p>
+    <p>Through the years I became the most experienced member of the
+      team where I supported my colleagues and tackled the most
+      challenging tasks as a senior developer and solution architect.</p>
+  </CareerBlock>
+}
+
+function CmJavaBlock() {
+  return <CareerBlock title="Java developer - CM" period={['April 2012', 'May 2017']} technologies={['Java EE', 'Struts']} className={styles.cmJava}>
+    <p>At CM I gained my first professional software development
+      experience while being part of various experienced teams creating
+      Java backends and internal web applications for processes within CM.</p>
+  </CareerBlock>
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { BlogPostList } from "@/components/blog-post/blog-post-list/BlogPostList"
 import { getAllBlogPosts, sortBlogPosts } from "./blog/blog.utils"
+import { DeLijnBlock } from "./career/page"
 
 export default async function HomePage() {
   const blogPosts = await getAllBlogPosts()
@@ -9,6 +10,8 @@ export default async function HomePage() {
   return <>
     <h2>Jonas Brems - Web developer</h2>
     <p>My very own place on the world wide web.</p>
+    <h3>Current position</h3>
+    <DeLijnBlock />
     <h3 style={{ marginTop: '2em' }}>Recent blog posts</h3>
     <BlogPostList blogPosts={recentBlogPosts} />
   </>
