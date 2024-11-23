cd22b15576a52c3c7115db02791a4406a86d928d
jbrems <jonasbrems@gmail.com>
2024-11-23 12:15:06 +0100

    Move Analytics to bottom of layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,11 +19,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <Analytics />
         <Navigation />
         <div style={{ padding: '0 1em' }}>
           {children}
         </div>
+        <Analytics />
       </body>
     </html>
   );
