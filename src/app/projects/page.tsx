f22cff9c66d5c204baead6037a67f5f361217a68
jbrems <jonasbrems@gmail.com>
2024-12-01 20:01:29 +0100

    Fix perspective and update images (4/4)

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -6,7 +6,7 @@ export default function ProjectsPage() {
   return <>
     <h2>Projects</h2>
     <div className={styles.projectsContainer}>
-      <Project url="/projects/presentr" coverImageUrl="/presentr.png" title="Presentr">
+      <Project url="/projects/presentr" coverImageUrl="/presentr.jpg" title="Presentr">
         <p>Present your photos with style!</p>
         <p>Implements the Google Photos picker APIs.</p>
       </Project>
