2c07c75981786f2aa8a2e7887b86705b0ba2cb9c
jbrems <jonasbrems@gmail.com>
2024-12-04 17:20:13 +0100

    Add game mechanics

--- src/components/combo-box/ComboBox.tsx
+++ src/components/combo-box/ComboBox.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { ChangeEvent, useEffect, useState } from "react"
+import { ChangeEvent, useEffect, useRef, useState } from "react"
 
 import styles from './ComboBox.module.css'
 
@@ -13,6 +13,7 @@ export function ComboBox({ options, selectOption }: { options: Option[], selectO
   const [value, setValue] = useState('')
   const [disabled, setDisabled] = useState(false)
   const [filteredOptions, setFilteredOptions] = useState<Option[]>([])
+  const inputElement = useRef<HTMLInputElement>(null)
   
   function handleInputChange(event: ChangeEvent<HTMLInputElement>) {
     setValue(event.target.value)
@@ -28,20 +29,24 @@ export function ComboBox({ options, selectOption }: { options: Option[], selectO
   }, [value, options, setFilteredOptions])
 
   useEffect(() => {
-    if (filteredOptions.length === 1) {
+    if (!disabled && filteredOptions.length === 1) {
       selectOption(filteredOptions[0])
       setValue(filteredOptions[0].label)
       setDisabled(true)
       setTimeout(() => { 
         setValue('')
         setDisabled(false)
-        document.getElementById('input')?.focus()
-      }, 500)
+        setFilteredOptions(options)
+      }, 999)
     }
-  }, [selectOption, filteredOptions])
+  }, [disabled, filteredOptions, selectOption, setValue, setDisabled, setFilteredOptions, options])
+
+  useEffect(() => {
+    if (!disabled) inputElement.current?.focus()
+  }, [disabled])
   
   return <div className={styles.comboBox}>
-      <input type="text" onChange={handleInputChange} value={value} className={styles.input} id="input" disabled={disabled} autoComplete="one-time-code"/>
+      <input type="text" onChange={handleInputChange} value={value} placeholder="Keep typing until only one option remains" className={styles.input} ref={inputElement} id="input" disabled={disabled} autoComplete="one-time-code"/>
       <div popover="manual" id="options" className={styles.options} >
         {filteredOptions.map(o => <Option key={o.value} label={o.label} markedCharacters={value.length} />)}
       </div>
