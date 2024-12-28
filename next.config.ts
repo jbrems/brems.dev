91ad1407de1894829b121ff157b36ec4c7238297
jbrems <jonasbrems@gmail.com>
2024-12-28 12:53:45 +0100

    Undo chrome-aws-lambda webpack override

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,5 @@
 import type { NextConfig } from 'next'
 
-const nextConfig: NextConfig = {
-  webpack: (config) => {
-    config.externals = [...config.externals, 'chrome-aws-lambda']
-    return config
-  },
-}
+const nextConfig: NextConfig = {}
 
 export default nextConfig
