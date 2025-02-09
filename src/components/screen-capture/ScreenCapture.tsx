19de6b3cffb69c16bca66046889e3e327ac61b74
jbrems <jonasbrems@gmail.com>
2025-02-09 16:32:39 +0100

    Fix project loading image detection

--- src/components/screen-capture/ScreenCapture.tsx
+++ src/components/screen-capture/ScreenCapture.tsx
@@ -1,19 +1,28 @@
 'use client'
 
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { ImageIcon } from '../icons/image-icon/ImageIcon'
 
 import styles from './ScreenCapture.module.css'
 
 export function ScreenCapture({ path, size, alt, className, ...props }: { path: string, size: 'portrait' | 'square', alt: string, className?: string } & React.ImgHTMLAttributes<HTMLImageElement>) {
   const [loading, setLoading] = useState(true)
+  const imgRef = useRef<HTMLImageElement>(null)
 
-  function handleLoad() {
-    setLoading(false)
-  }
+  // Periodically check if the image has been loaded
+  // img.onLoad does not fire when the image is loaded from the browser cache
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (imgRef?.current?.complete) {
+        setLoading(false)
+        clearInterval(interval)
+      }
+    }, 100)
+    return () => { clearInterval(interval) }
+  }, [setLoading])
 
   return <>
     {loading && <div className={`${styles.loadingPlaceholder} ${className}`}><ImageIcon /></div>}
-    <img src={`/api/screen-captures${path}?size=${size}`} onLoad={handleLoad} alt={alt} className={className} {...props} />
+    <img ref={imgRef} src={`/api/screen-captures${path}?size=${size}`} alt={alt} className={className} {...props} />
   </>
 }
