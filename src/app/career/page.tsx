a2685ac133ea68b8b4da95a993e25ecef964be63
jbrems <jonasbrems@gmail.com>
2025-02-09 23:11:13 +0100

    Career page improvements

--- src/app/career/page.tsx
+++ src/app/career/page.tsx
@@ -6,10 +6,22 @@ import Link from 'next/link'
 import { DeLijnBlock } from './DeLijnBlock'
 import { C4TLogo } from '@/components/logos/c4t-logo/C4TLogo'
 import { CmLogo } from '@/components/logos/cm-logo/CmLogo'
+import { screenCaptureSize } from '@/lib/puppeteer.service'
 
 export const metadata = {
-  title: 'Career',
+  title: 'My  career',
   description: 'A quick overview of my web developer career. For more details, see my Linkedin profile.',
+  openGraph: {
+    title: 'My career',
+    description: 'A quick overview of my web developer career. For more details, see my Linkedin profile.',
+    images: [{
+      url: 'https://brems.dev/api/screen-captures/career?size=openGraph',
+      type: 'image/jpeg',
+      width: screenCaptureSize.openGraph.width,
+      height: screenCaptureSize.openGraph.height,
+      alt: 'Screen capture of the career page',
+    }],
+  },
 }
 
 export default function CareerPage() {
@@ -43,21 +55,17 @@ function C4TBlock() {
 function CmWebBlock() {
   return <CareerBlock title="Web developer - CM" period={['May 2017', 'April 2022']} technologies={['Angular', 'Node.js', 'MongoDB', 'Express.js', 'OAuth', 'Jest', 'REST', 'Docker']} className={styles.cmWeb}>
     <CmLogo />
-    <p>After 5 years of Java development I joined CM&apos;s web team as a junior,
-      self-taught Angular and Nodejs
-      developer and contributed to making
-      web applications to be embedded in the company&apos;s website.</p>
-    <p>Through the years I became the most experienced member of the
-      team where I supported my colleagues and tackled the most
-      challenging tasks as a senior developer and solution architect.</p>
+    <p>In CM&apos;s web team I taught myself Angular and Nodejs development and contributed to making web applications to be embedded in the company&apos;s website and client portal (<a href="https://cm.be" target="_blank">cm.be</a>).</p>
+    <p>Through the years I became the most experienced member of the team in which I tackled the most challenging tasks and supported my colleagues as an senior developer and solution architect.</p>
+    <p>I am proud to have taken part in transitioning from a mono-repository architecture to a fast and scalable micro-frontend and -backend capable of handling traffic from the 1.5 million registered client accounts and over 1000 new account creations per day.</p>
+    <p>I was responsible for creating, maintaining and expanding the multi channel OAuth authentication framework allowing a secure and user friendly single sign on session on CM&apos;s mobile app, website and affiliated websites as well as the integration with the government&apos;s authentication platform (CSAM) using eID or ItsMe.</p>
   </CareerBlock>
 }
 
 function CmJavaBlock() {
   return <CareerBlock title="Java developer - CM" period={['April 2012', 'May 2017']} technologies={['Java EE', 'Struts', 'REST', 'SOAP']} className={styles.cmJava}>
     <CmLogo />
-    <p>At CM I gained my first professional software development
-      experience while being part of various experienced teams creating
-      Java backends and internal web applications for processes within CM.</p>
+    <p>At CM I gained my first professional software development experience while being part of various experienced teams creating Java backends and internal web applications for processes within CM.</p>
+    <p>During this time I discovered my love for frontend development which eventually led me to join CM’s web team as a full stack javascript developer while maintaining the Java backend service.</p>
   </CareerBlock>
 }
