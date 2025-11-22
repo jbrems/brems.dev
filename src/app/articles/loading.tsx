c9831118f84d3615ce9256cb811abaf8d56ec50e
Jonas Brems <jonasbrems@gmail.com>
2025-11-22 11:37:46 +0100

    Articles loading state

---
+++ src/app/articles/loading.tsx
@@ -0,0 +1,6 @@
+export default function ArticlesLoading() {
+  return <>
+    <h1>Articles</h1>
+    <p>Loading articles...</p>
+  </>
+}
--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,14 +1,16 @@
-'use server'
 import { ArticleList } from "@/components/article/ArticleList";
 import { getArticles } from "@/lib/article.service";
 
 export default async function Page() {
   const articles = await getArticles();
 
-  return (
-    <main>
-      <h1>Articles</h1>
-      <ArticleList articles={articles} />
-    </main>
-  );
+  return <>
+    <h1>Articles</h1>
+    <ArticleList articles={articles} />
+  </>
+}
+
+export const metadata = {
+  title: 'Articles',
+  description: 'An overview of all articles I have written and published. Mostly centered around web development.',
 }
